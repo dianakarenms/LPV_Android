c8463dd81a7ca73a66370f1c98033649ebb2760d
Diana Karen Muñoz Sierra <dianakarenms@gmail.com>
2017-05-28 03:52:13 -0500

    ApiConfig file both for debug and release

---
+++ app/src/debug/java/ApiConfig.java
@@ -0,0 +1,8 @@
+
+
+/**
+ * Created by clickaboom on 5/27/17.
+ */
+
+public class ApiConfig {
+}
---
+++ app/src/release/java/ApiConfig.java
@@ -0,0 +1,13 @@
+package com.clickaboom.letrasparavolar.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by clickaboom on 5/27/17.
+ */
+
+public static class ApiConfig {
+}
